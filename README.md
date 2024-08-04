c911c8ba79b3c5dc50b63d164eb5fae7f02bc784
marshrossney <17361029+marshrossney@users.noreply.github.com>
2024-08-04 15:23:50 +0100

    version 0.1.0 working locally

--- README.md
+++ README.md
@@ -1,3 +1,116 @@
 # Teams transcript formatter
 
+The purpose of this package is to make Microsoft Teams inverview transcripts easier to read and analyse using tools such as [QualCoder](https://github.com/ccbogel/QualCoder).
+
+Currently it is limited to one-to-one meetings with transcripts downloaded in the `.vtt` format.
+
+
+## Installation
+
+### From GitHub
+
+```sh
+$ python -m pip install git+https://github.com/jmarshrossney/teams-transcript-formatter
+```
+
+### From source
+
+If you want to make changes to the source code you can clone the repository and install in 'editable' mode,
+
+```sh
+$ git clone https://github.com/jmarshrossney/teams-transcript-formatter
+$ cd teams-transcript-formatter
+$ python -m pip install -e .
+```
+
+## Usage
+
+### Configuration
+
+You first need to create a file called `.env` in the directory in which you
+will be executing the script. This file should contain a single line with
+the form
+
+```sh
+INTERVIEWER='Interviewer Name'
+```
+
+where `Interviewer Name` should be replaced by the name of the interviewer
+*as it appears in the transcript*.
+
+Tip: you can do this directly from the command line by running `dotenv set INTERVIEWER "Interviewer Name"`.
+
+
+### Command-line tool
+
+There is one command-line script called `format-transcripts` which takes one or more `.vtt` files and produces one or more formatted files with the naming convention `<original_stem>_formatted.txt`. Optionally, you may also specify a directory for the formatted files using the `-o` flag (the default is the current working directory).
+
+
+You can also run `format-transcripts -h` (or `--help`) for guidance.
+
+
+## Example
+
+Say we have a Teams transcript file which we have downloaded and named `transcript.vtt` which looks something like this
+
+```sh
+$ head -11 transcript.vtt
+WEBVTT
+
+91b3f3c3-44c6-4a8b-8c0a-add105d816bd/32-0
+00:00:10.087 --> 00:00:13.130
+<v John Smith>Hello, I am the interviewer.</v>
+
+91b3f3c3-44c6-4a8b-8c0a-add105d816bd/32-1
+00:00:13.130 --> 00:00:16.270
+<v Jane Doe>Nice. I am the student being interviewed,
+and I have many things to say.</v>
+
+```
+
+We first need to set the interviewer name.
+
+```sh
+$ dotenv set INTERVIEWER "John Smith"
+```
+
+Now we can run the script and see what the formatted transcript looks like.
+
+```
+$ format-transcripts transcript.vtt
+$ head -6 transcript_formatted.txt
+Interviewer (00:10):
+        Hello, I am the interviewer.
+
+Student (00:13):
+        Nice. I am the student being interviewed, and I have many things to say.
+
+```
+
+## Privacy
+
+Although the names attached to the speakers are modified to read 'Interviewer'
+and 'Student', all other redactions of sensitive and identifiable information
+must be performed before running this script.
+
+Tip: the auto-generated transcripts can be edited in-situ using the Microsoft
+Stream app.
+
+Remember to delete the original transcripts after running this script!
+
+
+## Roadmap & contributing
+
+This is just something I threw together in a couple of hours because I needed it immediately and couldn't find anything similar elsewhere.
+
+There are some fairly simple additions that would make this more generally useful:
+
+- [ ] Handle meetings with >2 participants
+- [ ] User can configure how names are handled
+- [ ] Configure the output format, e.g. using a template
+- [ ] Handle Zoom meetings
+
+
+However, it's going to remain quite a low priority unless I can see it becoming useful to myself or colleagues.
+
 
---
+++ main.py
@@ -0,0 +1,141 @@
+"""
+Make Microsoft Teams inverview transcripts human-readable.
+"""
+
+from argparse import ArgumentParser
+from collections.abc import Iterable
+from pathlib import Path
+import re
+
+from dotenv import dotenv_values
+import pandas as pd
+
+
+class BadInterviewerName(Exception):
+    pass
+
+
+config = dotenv_values(".env")
+try:
+    INTERVIEWER = config["INTERVIEWER"]
+except KeyError as e:
+    raise BadInterviewerName(
+        "Please set `INTERVIEWER='Interviewer Name'` in `.env`"
+    ) from e
+
+parser = ArgumentParser(
+    prog="format_transcript",
+    description="Turns a `.vtt` audio transcript from Microsoft Teams/Stream into a human-readable plain text file.",
+)
+
+parser.add_argument(
+    "files",
+    type=Path,
+    nargs="+",  # at least one file must be provided
+    help="one or more `.vtt` files downloaded from Microsoft Teams/Stream",
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    type=Path,
+    help="directory in which to save the formatted `.txt` files",
+    default=".",
+)
+
+
+def _format_transcript(transcript: str) -> str:
+
+    # Strip first line which should just contain `WEBVTT`, then
+    # split the transcript into chunks of speech
+    chunks = transcript.split("\n\n")[1:]
+    array = [chunk.split("\n", maxsplit=2) for chunk in chunks]
+
+    df = pd.DataFrame(array, columns=["hash", "interval", "raw"])
+
+    # Replace hh:mm:ss.ff timestamp with mm:ss
+    df["timestamp"] = df["interval"].apply(
+        lambda s: ":".join(re.split(r":|\.", re.split(" ", s)[0])[1:3])
+    )
+
+    # Strip html tags and separate speaker/speech
+    df["raw"] = df["raw"].apply(lambda s: re.sub("<v |</v>", "", s))
+    df[["speaker", "speech"]] = df["raw"].str.split(">", n=1, expand=True)
+
+    # Replace newlines with spaces and drop rows containing no speech
+    df["speech"] = df["speech"].str.replace("\n", " ")
+    df = df[df["speech"].str.strip().astype(bool)]
+
+    df.drop(columns=["hash", "interval", "raw"], inplace=True)
+
+    # Merge adjacent blocks with the same speaker using a Boolean flag
+    # to indicate that the speaker has changed, then convert flag to
+    # integer increment using cumsum trick
+    df["block"] = (df["speaker"] != df["speaker"].shift()).cumsum()
+    df = df.groupby("block").agg(
+        {"timestamp": "first", "speaker": "first", "speech": lambda x: " ".join(x)}
+    )
+
+    # Check that there are 2 speakers, one of which is INTERVIEWER
+    speakers = df["speaker"].unique()
+    assert len(speakers) == 2
+    if INTERVIEWER not in speakers:
+        raise BadInterviewerName(
+            "Interviewer '{INTERVIEWER}' is not present in this transcript"
+        )
+
+    # Replace names with 'Interviewer' and 'Student'
+    df["speaker"] = df["speaker"].apply(
+        lambda name: "Interviewer" if name == INTERVIEWER else "Student"
+    )
+
+    # Format in human-readable way, appropriate for annotation
+    formatted_transcript = "\n\n".join(
+        [
+            f"{speaker} ({time}):\n\t{speech}"
+            for (time, speaker, speech) in df.itertuples(index=False, name=None)
+        ]
+    )
+
+    return formatted_transcript
+
+
+def main(files: list[Path], output_dir: Path) -> None:
+    """Format a given list of `.vtt` transcript files and save the results."""
+
+    assert isinstance(files, Iterable)
+    assert len(files) > 0
+    assert all([isinstance(file, Path) for file in files])
+    assert isinstance(output_dir, Path)
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    if not INTERVIEWER:
+        raise BadInterviewerName(
+            "Please set `INTERVIEWER` to the name of the interviewer as it appears in the transcript"
+        )
+
+    for infile in files:
+
+        # Read file as single string (assume it's sufficiently small)
+        with infile.open("r") as f:
+            raw_transcript = f.read()
+
+        formatted_transcript = _format_transcript(raw_transcript)
+
+        outfile = (output_dir / (infile.stem + "_formatted")).with_suffix(".txt")
+        assert not outfile.exists()
+
+        with outfile.open("w") as file:
+            file.write(formatted_transcript)
+
+        print(f"{infile} -> {outfile}")
+
+
+def cli():
+    """Wrapper around `main` that parses arguments from the command-line."""
+    args = parser.parse_args()
+    main(args.files, args.output)
+
+
+if __name__ == "__main__":
+    cli()
